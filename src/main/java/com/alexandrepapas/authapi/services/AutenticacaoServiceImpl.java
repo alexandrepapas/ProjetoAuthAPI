4254acfb4c8f930af4f81ed769cebcc453d56427
unknown <alehyxd@gmail.com>
2024-03-11 20:16:57 -0300

    criado autenticaoService

---
+++ src/main/java/com/alexandrepapas/authapi/services/AutenticacaoServiceImpl.java
@@ -0,0 +1,19 @@
+package com.alexandrepapas.authapi.services;
+
+import com.alexandrepapas.authapi.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(login);
+    }
+}
